2cdfb14473685e47235f784b1ff24e4191661f08
Armando Roman <armandoroman1016@gmail.com>
2019-08-21 10:09:44 -0700

    favicon replaced

--- breweries_api/src/components/LandingPage.js
+++ breweries_api/src/components/LandingPage.js
@@ -24,6 +24,7 @@ const mapStateToProps = state =>{
     return {
         breweries : state.breweries,
         fireRedirect : state.fireRedirect
+        
     }
 }
 export default connect(mapStateToProps, { getData })(LandingPage)
